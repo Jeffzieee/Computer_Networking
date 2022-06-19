9807868041f17bc6bf7216d7af653b4ceaabe504
Jephin John <jephinjohn85@gmail.com>
2022-06-19 18:57:30 +0530

    Distance Vector Routing Performed

--- Distance Vector Routing.c
+++ Distance Vector Routing.c
@@ -1,47 +1,41 @@
-//
-// Created by DarkGamerX100 on 6/19/2022.
-//
 #include<stdio.h>
-struct node
-{
-    unsigned dist[20];
-    unsigned from[20];
-}rt[10];
-int main()
-{
-    int dmat[20][20];
-    int n,i,j,k,count=0;
-    printf("\nEnter the number of nodes : ");
+
+struct node{
+    int dist[20];
+    int from[20];
+}router[10];
+
+void main(){
+    int n,i,j,k,cost_mat[20][20],counter=0;
+
+    printf("\nEnter the number of nodes: ");
     scanf("%d",&n);
-    printf("\nEnter the cost matrix :\n");
+
     for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-        {
-            scanf("%d",&dmat[i][j]);
-            dmat[i][i]=0;
-            rt[i].dist[j]=dmat[i][j];
-            rt[i].from[j]=j;
+        for(j=0;j<n;j++){
+            printf("\nEnter the distance of Node%d to Node%d: ",i+1,j+1);
+            scanf("%d",&cost_mat[i][j]);
+            cost_mat[i][i]=0;
+            router[i].dist[j]=cost_mat[i][j];
+            router[i].from[j]=j;
         }
-    do
-    {
-        count=0;
+
+    do{
+        counter=0;
         for(i=0;i<n;i++)
             for(j=0;j<n;j++)
                 for(k=0;k<n;k++)
-                    if(rt[i].dist[j]>dmat[i][k]+rt[k].dist[j])
-                    {
-                        rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-                        rt[i].from[j]=k;
-                        count++;
+                    if(router[i].dist[j]>cost_mat[i][k]+router[k].dist[j]){
+                        router[i].dist[j]=router[i].dist[k]+router[k].dist[j];
+                        router[i].from[j]=k;
+                        counter++;
                     }
-    }while(count!=0);
-    for(i=0;i<n;i++)
-    {
-        printf("\n\nState value for router %d is \n",i+1);
-        for(j=0;j<n;j++)
-        {
-            printf("\t\nnode %d via %d Distance%d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+    }while(counter!=0);
+
+    for(i=0;i<n;i++){
+        printf("\n\nState of Router%d is: ",i+1);
+        for(j=0;j<n;j++){
+            printf("\nNode%d through Node%d with Distance = %d",j+1,router[i].from[j]+1,router[i].dist[j]);
         }
     }
-    printf("\n\n");
 }
--- main.c
+++ main.c
@@ -1,6 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+struct node{
+    int dist[20];
+    int from[20];
+}router[10];
+
+void main(){
+   int n,i,j,k,cost_mat[20][20],counter=0;
+
+   printf("\nEnter the number of nodes: ");
+   scanf("%d",&n);
+
+   for(i=0;i<n;i++)
+       for(j=0;j<n;j++){
+         printf("\nEnter the distance of Node%d to Node%d: ",i+1,j+1);
+         scanf("%d",&cost_mat[i][j]);
+         cost_mat[i][i]=0;
+         router[i].dist[j]=cost_mat[i][j];
+         router[i].from[j]=j;
+       }
+
+   do{
+       counter=0;
+       for(i=0;i<n;i++)
+           for(j=0;j<n;j++)
+               for(k=0;k<n;k++)
+                   if(router[i].dist[j]>cost_mat[i][k]+router[k].dist[j]){
+                       router[i].dist[j]=router[i].dist[k]+router[k].dist[j];
+                       router[i].from[j]=k;
+                       counter++;
+                   }
+   }while(counter!=0);
+
+   for(i=0;i<n;i++){
+       printf("\n\nState of Router%d is: ",i+1);
+       for(j=0;j<n;j++){
+           printf("\nNode%d through Node%d with Distance = %d",j+1,router[i].from[j]+1,router[i].dist[j]);
+       }
+   }
 }
